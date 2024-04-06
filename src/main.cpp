c3046e6b54fcf3e74fd5b8068b9393fa4048e4f5
侠客令狐 <1042310516@qq.com>
2024-04-07 00:14:47 +0800

    OpenGL应用程序框架搭建，以及渲染一个简单的三角形。

---
+++ src/main.cpp
@@ -0,0 +1,125 @@
+// STL
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// Third-party library
+#include "vgl.h"
+#include "LoadShaders.h"
+
+/* 屏幕宽度 */
+static const int SCREEN_WIDTH = 1024;
+/* 屏幕高度 */
+static const int SCREEN_HEIGHT = 768;
+/* 屏幕清除颜色 */
+static const GLfloat CLEAR_COLOR[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+/* 窗口 */
+GLFWwindow* window;
+
+// 顶点数组对象
+GLuint vao = 0;
+
+// 顶点缓存对象
+GLuint vbo = 0;
+
+void preDraw() {
+  glClearBufferfv(GL_COLOR, 0, CLEAR_COLOR);
+}
+
+void Draw() {
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void initSetup() {
+  // 初始化glfw
+  if (glfwInit() == GLFW_FALSE) {
+    std::cout << "glfw library 初始化失败！" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  // 创建窗口
+  window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnMordenOpenGL", NULL, NULL);
+  glfwMakeContextCurrent(window);
+  // 初始化gl3w
+  gl3wInit();
+}
+
+void createVertexData() {
+  // 创建vao
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // 指定顶点数据
+  std::vector<GLfloat> vertices = { 
+    -0.5f, -0.5f, // 左下角
+    0.5f, -0.5f, // 右下角
+    0, 0.5f // 上方
+  };
+
+  // 创建vbo
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+
+  // 向vao解释attribute对应的缓存内容
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+  glDisableVertexAttribArray(0);
+}
+
+void createGraphicPipeline() {
+  GLint program = 0;
+  ShaderInfo  shaders[] =
+  {
+    { GL_VERTEX_SHADER, "shader/vert.glsl" },
+    { GL_FRAGMENT_SHADER, "shader/frag.glsl" },
+    { GL_NONE, NULL }
+  };
+
+  program = LoadShaders(shaders);
+  glUseProgram(program);
+}
+
+void mainLoop() {
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    preDraw();
+    Draw();
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+}
+
+void cleanUp() {
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
+
+int main() {
+  // 1.初始化设置
+  initSetup();
+  // 2.创建顶点数据
+  createVertexData();
+  // 3.创建图形管线
+  createGraphicPipeline();
+  // 4.主循环
+  mainLoop();
+  // 5.清理
+  cleanUp();
+  return 0;
+}
