f9510e6acd9a61f4924bf11527e969343278966d
yanchong <1042310516@qq.com>
2024-04-10 10:52:45 +0800

    assimp库漏提交dll文件，同时更新生成后处理脚本，在生成后复制dll到bin文件夹下。

--- post_build_event.bat
+++ post_build_event.bat
@@ -1,8 +1,10 @@
 @echo off
 ::项目生成后事件
 
-::删除bin路径下的data文件夹
-if EXIST bin\data (rd bin\data /s /q)
+::1.拷贝data文件夹下的内容到bin文件夹下，
+::如果bin文件夹下不存在data文件夹，/i指令会自动创建。
+::/d检查是否有更新的数据，如果有则覆盖。
+Xcopy data bin\data /d /y /e /s /i
 
-::拷贝data文件夹下的内容到bin文件夹下，如果bin文件夹下不存在data文件夹，/i指令会自动创建
-Xcopy data bin\data /e /s /i
+::2.拷贝dll文件夹下的内容到bin文件夹下，如果有更新的dll，则覆盖。
+Xcopy dll bin /d /y
